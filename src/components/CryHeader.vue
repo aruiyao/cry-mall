8463c23fbceac50968806aef7633240a4f7de7b9
aruiyao <1023934747@qq.com>
2020-06-13 15:30:41 +0800

    初始化

---
+++ src/components/CryHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'CryHeader'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/MainFrame.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'MainFrame'
+}
+</script>
+
+<style scoped>
+
+</style>
